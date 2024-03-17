cd518bf1015d76c8598faca0b5c2b847a1d5e631
hsilasss <hsilassamban@gmail.com>
2024-03-17 18:34:27 +0700

    merging

---
+++ MOD5_1302220143/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD5_1302220143
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            HaloGeneric.SapaUser("Jeremy Mathew Fabian Sitepu");
+            DataGeneric<String> NIM = new DataGeneric<String>("1302220143");
+            NIM.PrintData();
+        }
+    }
+
+    public class DataGeneric<T>
+    {
+        T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+
+    public class HaloGeneric
+    {
+        public static void SapaUser(String X)
+        {
+            Console.WriteLine("Halo user " + X);
+        }
+    }
+}
